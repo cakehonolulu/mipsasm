9699e5fbf193df4f15ac49dbc441c6115c88ff6c
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2025-04-24 23:48:20 +0200

    ast: Fix warning

--- src/ast.rs
+++ src/ast.rs
@@ -635,7 +635,7 @@ impl fmt::Display for Instruction {
                 e => panic!("{:?} not implemented", e),
             },
             Instruction::Bytes { bytes } => write!(f, "{:11}0x{:08x}", ".word", bytes),
-            e => panic!("Invalid instruction: {:?}", 0),
+            _ => panic!("Invalid instruction: {:?}", 0),
         }
     }
 }
